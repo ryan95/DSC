fbdfb9fd8e562690af3061dfe9dfccf90db0892a
Ryan Stephens <ryan.stephens@ocsl.co.uk>
2020-03-30 11:58:36 +0100

    Updated AD Connect Script

--- ADConnect.ps1
+++ ADConnect.ps1
@@ -1,3 +1,4 @@
 $URL = $args[0]
 mkdir C:\ADConnect
 Invoke-WebRequest -Uri $URL -UseBasicParsing -OutFile "C:\ADConnect\ADConnect.msi"
+Start-Process msiexec -Wait -ArgumentList '/I C:\ADConnect\ADConnect.msi /quiet'
