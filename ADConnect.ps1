19b645bb85807d8fac109b24d305889fb7f69ed3
Ryan Stephens <ryan.stephens@ocsl.co.uk>
2020-03-30 11:14:43 +0100

    Updated AD Connect Script

--- ADConnect.ps1
+++ ADConnect.ps1
@@ -1,4 +1,3 @@
 $URL = $args[0]
-$username = $args[1]
-$path = "C:\Users\" + $username + "\Desktop\ADConnect.msi"
-Invoke-WebRequest -Uri $URL -UseBasicParsing -OutFile $path
+mkdir C:\ADConnect
+Invoke-WebRequest -Uri $URL -UseBasicParsing -OutFile "C:\ADConnect"
